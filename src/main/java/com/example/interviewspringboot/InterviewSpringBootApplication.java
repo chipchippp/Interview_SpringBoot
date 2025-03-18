338f4f6ce5129588e45265f0c554f3cb0e441444
chipchippp <114909102+chipchippp@users.noreply.github.com>
2025-03-18 16:33:11 +0700

    commit

---
+++ src/main/java/com/example/interviewspringboot/InterviewSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.interviewspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewSpringBootApplication.class, args);
+    }
+
+}
